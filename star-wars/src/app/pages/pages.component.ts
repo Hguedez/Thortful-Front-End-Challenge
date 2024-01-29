10e7949fc6fb254b828b4a98adab22a82116f836
HG-SL <hilery@shocklogic.com>
2024-01-29 00:57:43 -0400

    reduce repeated code

--- star-wars/src/app/pages/pages.component.ts
+++ star-wars/src/app/pages/pages.component.ts
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
     styleUrls: ['./pages.component.scss']
 })
 export class PagesComponent {
+  episodes = Array.from({length: 6}, (_, i) => `episode${i + 1}`);
+
   constructor(private router: Router) {}
 
   /**
